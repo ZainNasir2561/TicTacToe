fafd5f08398422ae36fef630c919402ff826d7fe
Zain Nasir <102922448+ZainNasir2561@users.noreply.github.com>
2024-01-20 12:28:13 -0500

    Add files via upload

---
+++ src/components/GameBoard.jsx
@@ -0,0 +1,22 @@
+export default function GameBoard({ onSelectSquare, board }) {
+    return (
+    <ol id="game-board">
+      {board.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((playerSymbol, colIndex) => (
+              <li key={colIndex}>
+                <button
+                  onClick={() => onSelectSquare(rowIndex, colIndex)}
+                  disabled={playerSymbol !== null}  // disable the button if theres a symbol on the button
+                >
+                  {playerSymbol}
+                </button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
+    </ol>
+  );
+}
